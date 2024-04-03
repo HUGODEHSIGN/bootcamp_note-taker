069dcffe97f8daad1010285293030556b73747c7
Hugo Hsi <hhsi@hugodehsign.com>
2024-04-03 16:46:29 -0400

    flushed out post route, allowing user to post new notes to db.json

--- public/routes/notes.js
+++ public/routes/notes.js
@@ -1,13 +1,30 @@
 import express from 'express';
+import uuid from '../../helpers/uuid.js';
+import {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} from '../../helpers/fsUtils.js';
 
 const notes = express.Router();
 
 notes.get('/', (req, res) => {
-  console.log('read from /api/notes');
+  readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post('/', (req, res) => {
-  console.log('post to /api/notes');
+  console.log('post to /api/notes', req.body);
+  const { title, text } = req.body;
+  const newNote = {
+    id: uuid(),
+    title,
+    text,
+  };
+  readAndAppend(newNote, './db.json');
+});
+
+notes.delete('/:id', (req, res) => {
+  console.log('delete note', req.params);
 });
 
 export default notes;
